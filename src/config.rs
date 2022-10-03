e6b376caac019c8e8120ccbb07591d83a605e7a5
ShadowCurse <shadowcurse57@gmail.com>
2022-10-03 18:25:08 +0300

    container config constructor

--- src/config.rs
+++ src/config.rs
@@ -9,3 +9,20 @@ pub struct ContainerOpts {
     pub uid: u32,
     pub mount_dir: PathBuf,
 }
+
+impl ContainerOpts {
+    pub fn new(command: String, uid: u32, mount_dir: PathBuf) -> ContainerOpts {
+        let argv: Vec<CString> = command
+            .split_ascii_whitespace()
+            .map(|s| CString::new(s).expect("Cannot read arg"))
+            .collect();
+        let path = argv[0].clone();
+
+        ContainerOpts {
+            path,
+            argv,
+            uid,
+            mount_dir,
+        }
+    }
+}
