6e32ce41ed386f026f375807e52df0334aa3f10f
ShadowCurse <shadowcurse57@gmail.com>
2022-10-04 20:42:33 +0300

    added errors to container config + basic tests

--- src/config.rs
+++ src/config.rs
@@ -1,28 +1,55 @@
 use std::ffi::CString;
 use std::path::PathBuf;
 
-#[derive(Clone)]
-pub struct ContainerOpts {
-    pub path: CString,
+#[derive(Debug, thiserror::Error, PartialEq)]
+pub enum Error {
+    #[error("No binary path provided in the commands")]
+    NoBinayPath,
+}
+
+#[derive(Debug, Clone)]
+pub struct ContainerConfig {
+    pub binary_path: CString,
     pub argv: Vec<CString>,
 
     pub uid: u32,
     pub mount_dir: PathBuf,
 }
 
-impl ContainerOpts {
-    pub fn new(command: String, uid: u32, mount_dir: PathBuf) -> ContainerOpts {
-        let argv: Vec<CString> = command
+impl ContainerConfig {
+    pub fn new(command: String, uid: u32, mount_dir: PathBuf) -> Result<Self, Error> {
+        let argv = command
             .split_ascii_whitespace()
             .map(|s| CString::new(s).expect("Cannot read arg"))
-            .collect();
-        let path = argv[0].clone();
+            .collect::<Vec<_>>();
 
-        ContainerOpts {
-            path,
+        if argv.is_empty() {
+            return Err(Error::NoBinayPath);
+        }
+
+        let binary_path = argv[0].clone();
+
+        Ok(Self {
+            binary_path,
             argv,
             uid,
             mount_dir,
-        }
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn container_config_new() {
+        assert!(ContainerConfig::new("command".to_string(), 0, "/mount".into()).is_ok());
+        assert_eq!(
+            ContainerConfig::new("".to_string(), 0, "/mount".into())
+                .err()
+                .unwrap(),
+            Error::NoBinayPath
+        );
     }
 }
